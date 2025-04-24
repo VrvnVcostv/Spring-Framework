9824d4dc03432c44939cfaa247da2c66b20f4bff
Bruno <brunoacosta903@gmail.com>
2025-04-24 13:35:10 +0200

    Added the SecondSection project

---
+++ SecondSection/src/main/java/com/udemy/secondsection/di/SecondSection/SecondSectionApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.secondsection.di.SecondSection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSectionApplication.class, args);
+	}
+
+}
---
+++ SecondSection/src/test/java/com/udemy/secondsection/di/SecondSection/SecondSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.secondsection.di.SecondSection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
