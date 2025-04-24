bcb101ec27da82b593182067bc795f9645f2f407
Bruno <brunoacosta903@gmail.com>
2025-04-24 14:12:07 +0200

    Implemented findAll in ProductService

---
+++ SecondSection/src/main/java/com/udemy/secondsection/di/SecondSection/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.udemy.secondsection.di.SecondSection.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.udemy.secondsection.di.SecondSection.models.Product;
+import com.udemy.secondsection.di.SecondSection.repositories.ProductRepository;
+
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        return productRepository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice() * 1.25d;
+            p.setPrice(priceImp.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id){
+        return null;
+    }
+}
