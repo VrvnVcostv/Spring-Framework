6d0e93aefbbcb7105bb475c12babed802067942b
Bruno <brunoacosta903@gmail.com>
2025-04-26 20:07:15 +0200

    Added the ThirdSection project

---
+++ ThirdSection/src/main/java/com/udemy/thirdsection/invoice/ThirdSection/ThirdSectionApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.thirdsection.invoice.ThirdSection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdSectionApplication.class, args);
+	}
+
+}
---
+++ ThirdSection/src/test/java/com/udemy/thirdsection/invoice/ThirdSection/ThirdSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.thirdsection.invoice.ThirdSection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
