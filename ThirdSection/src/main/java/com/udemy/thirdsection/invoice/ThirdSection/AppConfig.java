11f94302547d9cb9f43a43116ffc896352edcc84
Bruno <brunoacosta903@gmail.com>
2025-04-26 23:06:05 +0200

    @PostContruct decorator

--- ThirdSection/src/main/java/com/udemy/thirdsection/invoice/ThirdSection/AppConfig.java
+++ ThirdSection/src/main/java/com/udemy/thirdsection/invoice/ThirdSection/AppConfig.java
@@ -17,11 +17,22 @@ import java.util.Arrays;
 })
 public class AppConfig {
 
+    //@Primary
     @Bean
     List<Item> itemsInvoice(){
         Product p1 = new Product("Sony Camera",800);
         Product p2 = new Product("Bicycle",26);
-        Product p3 = new Product("Wifi Router",99);
+        Product p3 = new Product("Sport shoes",99);
         return Arrays.asList(new Item(p1,2), new Item(p2,5), new Item(p3,7));
+    }
+
+    @Bean("default") // To make the change without modifying too much code, you can either use the @Primary annotation or set a bean as the ("default").
+    List<Item> itemsInvoiceOfOffice(){
+        Product p1 = new Product("Monitor",200);
+        Product p2 = new Product("Chair",400);
+        Product p3 = new Product("Desk",100);
+        Product p4 = new Product("Lamp",29);
+        Product p5 = new Product("PC",399);
+        return Arrays.asList(new Item(p1,2), new Item(p2,5), new Item(p3,7), new Item(p4,3), new Item(p5,1));
     }
 }
--- ThirdSection/src/main/java/com/udemy/thirdsection/invoice/ThirdSection/models/Invoice.java
+++ ThirdSection/src/main/java/com/udemy/thirdsection/invoice/ThirdSection/models/Invoice.java
@@ -3,9 +3,11 @@ package com.udemy.thirdsection.invoice.ThirdSection.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,13 +17,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Component
 public class Invoice {
+
+
     @Autowired
     private Client client;
     @Value("${invoice.description}")
     private String description;
     @Autowired
+    @Qualifier("default")
     private List<Item> items; 
     
+    @PostConstruct
+    public void init(){
+        System.out.println("Creando el compoente de la factura");
+        client.setName(client.getName().concat(" Pepe"));
+        description = description.concat(" of the client: ").concat(client.getName().concat(" ").concat(client.getLastname()));
+
+    }
+
     public int getTotal(){
         int total = 0;
         for (Item item : items) {
