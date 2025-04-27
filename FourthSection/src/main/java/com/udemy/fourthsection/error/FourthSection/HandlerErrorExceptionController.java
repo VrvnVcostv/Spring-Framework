ccdf50c01f03109ed469956396496d2ed64a2aaf
Bruno <brunoacosta903@gmail.com>
2025-04-27 19:54:43 +0200

    Custom NumberFormatException

--- FourthSection/src/main/java/com/udemy/fourthsection/error/FourthSection/HandlerErrorExceptionController.java
+++ FourthSection/src/main/java/com/udemy/fourthsection/error/FourthSection/HandlerErrorExceptionController.java
@@ -2,11 +2,14 @@ package com.udemy.fourthsection.error.FourthSection;
 
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.udemy.fourthsection.error.FourthSection.models.Error;
@@ -35,4 +38,16 @@ public class HandlerErrorExceptionController {
         // return ResponseEntity.notFound().build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String, Object>> numberFormatEx(Exception ex){
+        Map<String, Object> error = new HashMap<>(); 
+        error.put("date", new Date());
+        error.put("error", "Number not valid");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.internalServerError().body(error);
+    }
+
 }
