27487780128e3912c68b1fd2a0fa1dd8fd73b40c
Bruno <brunoacosta903@gmail.com>
2025-04-28 11:58:46 +0200

    Added the FifthSection project

---
+++ FifthSection/src/main/java/com/udemy/fifthsection/interceptor/FifthSection/FifthSectionApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.fifthsection.interceptor.FifthSection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifthSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifthSectionApplication.class, args);
+	}
+
+}
---
+++ FifthSection/src/test/java/com/udemy/fifthsection/interceptor/FifthSection/FifthSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.fifthsection.interceptor.FifthSection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifthSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
