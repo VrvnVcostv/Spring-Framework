39af438d201c300a6c3c7991306bf429e2f48b7f
Bruno <brunoacosta903@gmail.com>
2025-04-29 01:16:52 +0200

    ElapsedTime implemented

--- FifthSection/src/main/java/com/udemy/fifthsection/interceptor/FifthSection/interceptors/LoadingTimeInterceptor.java
+++ FifthSection/src/main/java/com/udemy/fifthsection/interceptor/FifthSection/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.udemy.fifthsection.interceptor.FifthSection.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -16,17 +18,21 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+    throws Exception {
+        logger.info("LoadingTimeInterceptor: preHandle() entrando..." + ((HandlerMethod)handler).getMethod().getName());
+        request.setAttribute("start", System.currentTimeMillis());
+        Random delay = new Random();
+        Thread.sleep(delay.nextInt(500));
+        return true;
+    }
+    
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-                logger.info("LoadingTimeInterceptor: postHandle() saliendo..." + ((HandlerMethod)handler).getMethod().getName());
+            System.out.println("Its been: " + (System.currentTimeMillis() - (long) request.getAttribute("start") + " ms"));
+            logger.info("LoadingTimeInterceptor: postHandle() saliendo..." + ((HandlerMethod)handler).getMethod().getName());
     }
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-                logger.info("LoadingTimeInterceptor: preHandle() entrando..." + ((HandlerMethod)handler).getMethod().getName());
-        return true;
-    }
-
 }
