ddbb31f907188a372fdd7c59fe5dd6539ff2bcf1
Bruno <brunoacosta903@gmail.com>
2025-04-30 00:52:16 +0200

    GreetingService creation

--- SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/controllers/GreetingController.java
+++ SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/controllers/GreetingController.java
@@ -2,16 +2,22 @@ package com.udemy.seventhsection.aop.SeventhSection.controllers;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.udemy.seventhsection.aop.SeventhSection.services.GreetingService;
+
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("greeting")
     public ResponseEntity<?> greeting(){
-        return ResponseEntity.ok(Collections.singletonMap("greetings", null));
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("Pepe", "Hi how are you")));
     }
 
 }
---
+++ SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/services/GreetingService.java
@@ -0,0 +1,5 @@
+package com.udemy.seventhsection.aop.SeventhSection.services;
+
+public interface GreetingService {
+    String sayHello(String person, String phrase);
+}
