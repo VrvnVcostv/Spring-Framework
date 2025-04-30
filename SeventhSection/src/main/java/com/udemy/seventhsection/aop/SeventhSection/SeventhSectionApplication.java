0ff66c8d3fccbea27f37122612f711205256762b
Bruno <brunoacosta903@gmail.com>
2025-04-30 19:48:17 +0200

    @Before and @After decorators on @Aspect

--- SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/SeventhSectionApplication.java
+++ SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/SeventhSectionApplication.java
@@ -2,7 +2,6 @@ package com.udemy.seventhsection.aop.SeventhSection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 // @EnableAspectJAutoProxy
 @SpringBootApplication
--- SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/aop/GreetingAspect.java
+++ SeventhSection/src/main/java/com/udemy/seventhsection/aop/SeventhSection/aop/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.udemy.seventhsection.aop.SeventhSection.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,11 +16,18 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.udemy.seventhsection.aop.SeventhSection.services.GreetingService.sayHello(..))")
+    @Before("execution(* com.udemy.seventhsection.aop.SeventhSection.services.GreetingService.*(..))")
     public void loggerBefore(JoinPoint joinPoint){
         String metod = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Before: " + metod + " with the args " + args);
     }
 
+    @After("execution(* com.udemy.seventhsection.aop.SeventhSection.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint){
+        String metod = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("After: " + metod + " with the args " + args);
+    }
+    
 }
