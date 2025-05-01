416514ac2d62dea40da6eac40d8fe73431de527b
Bruno <brunoacosta903@gmail.com>
2025-05-01 17:23:14 +0200

    Added the EighthSection project

---
+++ EighthSection/src/main/java/com/udemy/eighthsection/jpa/EighthSection/EighthSectionApplication.java
@@ -0,0 +1,19 @@
+package com.udemy.eighthsection.jpa.EighthSection;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EighthSectionApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EighthSectionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
---
+++ EighthSection/src/test/java/com/udemy/eighthsection/jpa/EighthSection/EighthSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.eighthsection.jpa.EighthSection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EighthSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
