9e42dc6988d474003d7fc0de8b0851a11112ef9e
Bruno <brunoacosta903@gmail.com>
2025-05-03 19:55:22 +0200

    Added the NinthSection project

---
+++ NinthSection/src/main/java/com/udemy/ninthsection/jpa/NinthSection/NinthSectionApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.ninthsection.jpa.NinthSection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinthSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinthSectionApplication.class, args);
+	}
+
+}
---
+++ NinthSection/src/test/java/com/udemy/ninthsection/jpa/NinthSection/NinthSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.ninthsection.jpa.NinthSection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinthSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
