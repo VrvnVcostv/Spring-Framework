af573df8b008a5b2971d8d7de8afea3053a6a9fc
Bruno <brunoacosta903@gmail.com>
2025-05-07 13:37:03 +0200

    Added the TenthSection project

---
+++ TenthSection/src/main/java/com/udemy/tenthsection/crud/TenthSection/TenthSectionApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.tenthsection.crud.TenthSection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenthSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenthSectionApplication.class, args);
+	}
+
+}
---
+++ TenthSection/src/test/java/com/udemy/tenthsection/crud/TenthSection/TenthSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.tenthsection.crud.TenthSection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenthSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
