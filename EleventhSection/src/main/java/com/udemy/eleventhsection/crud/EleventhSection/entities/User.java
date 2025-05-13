9b796aa82691462482b001917e6902538e16e4c1
Bruno <brunoacosta903@gmail.com>
2025-05-13 14:37:23 +0200

    Bidirectional relationship between User and Role

--- EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/entities/User.java
+++ EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/entities/User.java
@@ -1,7 +1,9 @@
 package com.udemy.eleventhsection.crud.EleventhSection.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -46,10 +48,19 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean admin;
 
+    @JsonIgnoreProperties({"users", "handler", "hibernateLazyInitializer"})
     @ManyToMany
-    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
-            @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
+    @JoinTable(
+        name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), 
+        inverseJoinColumns = @JoinColumn(name = "role_id"), 
+        uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
     private List<Role> roles;
+
+
+    
+    public User() {
+        roles = new ArrayList();
+    }
 
     public Long getId() {
         return id;
@@ -98,6 +109,37 @@ public class User {
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
-    
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+    
+    
 }
