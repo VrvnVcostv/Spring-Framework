89a572d335f2d893adaf6e51ec8fabe7a8cfd9fa
Bruno <brunoacosta903@gmail.com>
2025-05-13 14:11:54 +0200

    /Register path created with more security

--- EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/controllers/UserController.java
+++ EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/controllers/UserController.java
@@ -32,11 +32,20 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
+    private ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result){
         if(result.hasFieldErrors()){
             return validation(result);
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result){
+        if(result.hasFieldErrors()){
+            return validation(result);
+        }
+        user.setAdmin(false);
+        return create(user, result);
     }
 
     private ResponseEntity<?> validation(BindingResult result) {
--- EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/security/SpringSecurityConfig.java
+++ EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.udemy.eleventhsection.crud.EleventhSection.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,7 +20,8 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests((authz) -> authz
-        .requestMatchers("/api/user").permitAll()
+        .requestMatchers(HttpMethod.GET,"/api/user").permitAll()
+        .requestMatchers(HttpMethod.POST,"/api/user/register").permitAll()
         .anyRequest().authenticated())
         .csrf(config -> config.disable())
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
