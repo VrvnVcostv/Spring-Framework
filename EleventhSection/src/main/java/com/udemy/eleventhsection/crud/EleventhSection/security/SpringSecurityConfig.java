ce6f561c4fe632dc61ce8ca39e6e0f016fed731b
Bruno <brunoacosta903@gmail.com>
2025-05-17 21:34:19 +0200

    URL and HTTP methods assigned to roles

--- EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/security/SpringSecurityConfig.java
+++ EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/security/SpringSecurityConfig.java
@@ -37,6 +37,11 @@ public class SpringSecurityConfig {
         return http.authorizeHttpRequests((authz) -> authz
         .requestMatchers(HttpMethod.GET,"/api/user").permitAll()
         .requestMatchers(HttpMethod.POST,"/api/user/register").permitAll()
+        .requestMatchers(HttpMethod.POST,"/api/user").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.POST,"/api/products").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.GET,"/api/products", "/api/products/{id}").hasAnyRole("ADMIN","USER")
+        .requestMatchers(HttpMethod.PUT,"/api/products/{id}").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.DELETE,"/api/products/{id}").hasRole("ADMIN")
         .anyRequest().authenticated())
         .addFilter(new JwtAuthenticactionFilter(authenticationManager()))
         .addFilter(new JwtValidationFilter(authenticationManager()))
