878463e63b9dc2ed1df4c0174ffd85a79fba704d
Bruno <brunoacosta903@gmail.com>
2025-05-19 23:24:16 +0200

    Eleventh Section Jar Created

--- EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/EleventhSectionApplication.java
+++ EleventhSection/src/main/java/com/udemy/eleventhsection/crud/EleventhSection/EleventhSectionApplication.java
@@ -3,6 +3,7 @@ package com.udemy.eleventhsection.crud.EleventhSection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Jar Created
 @SpringBootApplication
 public class EleventhSectionApplication {
 
